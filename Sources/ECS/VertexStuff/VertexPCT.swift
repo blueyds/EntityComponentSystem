615745db5366ca5fabb1a6e1145d06f7deb13702
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-12-10 07:37:16 -0600

    removed dependency for gamecollor

--- Sources/ECS/VertexStuff/VertexPCT.swift
+++ Sources/ECS/VertexStuff/VertexPCT.swift
@@ -10,10 +10,10 @@ import simd
 
 public struct VertexPCT:VertexProtocol{
 	public let position: SIMD3<Float>
-	public let color: GameColor
+	public let color: SIMD4<Float>
 	public let texCoord: SIMD2<Float>
 
-	public init(x: Float, y: Float, u: Float, v: Float, color: GameColor){
+	public init(x: Float, y: Float, u: Float, v: Float, color: SIMD4<Float>){
 		position = SIMD3(x, y, 0)
 		texCoord = SIMD2(u, v)
 		self.color = color
