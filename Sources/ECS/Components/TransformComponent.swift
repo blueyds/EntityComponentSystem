b067db5f80ad37067c11c2527526cddf5f22bee5
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-12-09 22:41:22 -0600

    error

--- Sources/ECS/Components/TransformComponent.swift
+++ Sources/ECS/Components/TransformComponent.swift
@@ -4,7 +4,7 @@ public class TransformComponent: Component{
 	public var entity: Entity? = nil
 	static public var typeID: Int = Manager.getNewComponentTypeID()
 	var transforms: TransformData = TransformData()
-	var modelMatrix: Matrix { transforms?.modelMatrix ?? .identity}
+	var modelMatrix: Matrix { transforms.modelMatrix }
 
 }
 
