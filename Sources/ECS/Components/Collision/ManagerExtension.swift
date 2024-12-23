5373eb5ff6bfb0c785bbcc26454380f9c25e10b3
Blueyds <9055449+blueyds@users.noreply.github.com>
2024-12-23 17:22:46 -0600

    modified collision test

--- Sources/ECS/Components/Collision/ManagerExtension.swift
+++ Sources/ECS/Components/Collision/ManagerExtension.swift
@@ -1,12 +1,19 @@
 extension Manager{
-	
-	public func findCollisions(for entity: Entity, using testFn: (CollisionComponent,CollisionComponent)->Bool)->[CollisionComponent]{
+	public typealias CollisionTestFn = (CollisionComponent,CollisionComponent)->Bool
+	public func findCollisions(for entity: Entity, using testFn: CollisionTestFn )->[CollisionComponent]{
 		var result: [CollisionComponent] = []
 		guard let colA = entity.getComponent(ofType: CollisionComponent.self) else { return result }
 		self.forEach(){
-			if let col = $0.getComponent(ofType: CollisionComponent.self),
-				testFn(colA, col){
-				result.append(col)
+			testCollision(colA: colA, entityB: $0, using: testFn(::))
+		}
+		return result
+	}
+	private func testCollision(colA: CollisionComponent, entityB: Entity, using testFn: CollisionTestFn-> CollisionComponent?{
+		var result: CollisionComponent? = nil
+		if let colB = entityB.getComponent(ofType: CollisionComponent.self),
+			testFn(colA, colB){
+			if colA.id != colB.id{
+				result = colB
 			}
 		}
 		return result
