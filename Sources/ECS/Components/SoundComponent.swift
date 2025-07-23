55a5ee1b6bdf39a31dd97e1d265e119355037050
Blueyds <9055449+blueyds@users.noreply.github.com>
2025-07-22 21:20:06 -0500

    SoundComponent

---
+++ Sources/ECS/Components/SoundComponent.swift
@@ -0,0 +1,28 @@
+import ECS
+import AVFoundation
+
+public class SoundComponent: Component{
+    public var entity: (any Entity)!
+    public static let typeID: Int = Manager.getNewComponentTypeID()
+    private let file: String
+    private var sound: AVAudioPlayer? = nil
+    public init(named: String){
+        file = named
+    }
+    public func setup() {
+        let path = Bundle.main.path(forResource: file, ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+        
+       do {
+           sound = try AVAudioPlayer(contentsOf: url)
+           sound?.play()
+        } catch {
+            // couldn't load file :(
+        }
+    } 
+    public func destroy() {
+        sound?.stop()
+    }
+    
+}
+
